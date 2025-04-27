48621e0784fe33800c35c47cdf2bfb30a4336414
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-04-27 16:14:44 +0300

    init: create initial classes

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import socket
+import threading
+import hashlib
+import pickle
+from tqdm import tqdm
+from time import sleep
+
+CHUNK_SIZE = 1024 * 1024
+TRACKER_PORT = 5000
+PEER_PORT_BASE = 6000
+
+class Tracker:
+    pass
+
+class Peer:
+    pass
