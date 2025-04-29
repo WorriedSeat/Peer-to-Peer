4610077d0fa622efafeb24d0abf9b8567b8187dd
Sarantsev <stepansarantsev1@gmail.com>
2025-04-29 22:51:33 +0300

    Refactor: all works properly

--- DHT_node.py
+++ DHT_node.py
@@ -5,10 +5,9 @@ from time import time, sleep
 from random import random, choice
 
 
-PARALLEL_REQUESTS = 3 # Requests parallelization
 ROUTING_REFRESH_INTERVAL = 1800  # Refreshing routing table
-ROUTING_TABLE_SIZE = 10 # Number of records in routing tables
 CLEANUP_REFRESH_INTERVAL = 1800 # Refreshing storage
+ROUTING_TABLE_SIZE = 10 # Number of records in routing tables
 
 
 # Info about dht nodes
@@ -51,6 +50,11 @@ class DHTNode:
         self.running = True
         # Logs
         print(f"Node {self.node_id.hex()[:8]} running at {ip}:{port}")
+    
+    # Stop running dht node
+    def shutdown(self): 
+        self.running = False
+        self.socket.close()
 
     # Kademlia XOR distance measure
     def distance(self, id1, id2):
@@ -70,6 +74,7 @@ class DHTNode:
         if len(self.routing_table) < ROUTING_TABLE_SIZE:
             self.routing_table[node_id] = new_node
         else:
+            # Replace the oldest node
             last_time = time()
             last_node = None
             for node in self.routing_table.keys():
@@ -78,11 +83,32 @@ class DHTNode:
                     last_node = node
             self.routing_table.pop(last_node)
             self.routing_table[node_id] = new_node
+    
+    # Connect to the dht net using bootstrap node
+    def bootstrap(self, bootstrap_nodes):
+        for node in bootstrap_nodes:
+            self.find_node(self.node_id, node)
 
     # Start listening for queries and refreshing referencing table
     def start(self):
-        Thread(target=self.listen).start()
         Thread(target=self.refresh).start()
+        Thread(target=self.listen).start()
+    
+    # Periodically refresh routing table
+    def refresh(self):
+        while self.running:
+            sleep(ROUTING_REFRESH_INTERVAL)
+            # Refresh a random node
+            rnd_node = self.routing_table[choice(self.routing_table.keys())]
+            self.find_node(rnd_node.id, (rnd_node.ip, rnd_node.port))
+    
+    # Delete old peers from storage
+    def cleanup_storage(self):
+        now = time()
+        for file_hash in self.storage.keys():
+            self.storage[file_hash] = [p for p in self.storage[file_hash] if now - p.last_seen < CLEANUP_REFRESH_INTERVAL]
+            if len(self.storage[file_hash]) == 0:
+                self.storage.pop(file_hash)
 
     # Listen for messages
     def listen(self):
@@ -102,7 +128,6 @@ class DHTNode:
             if msg[0] == "PING":
                 self.socket.sendto(b"PONG", addr)
             elif msg[0] == "FIND_NODE":
-                # print("Find node", self.port)
                 target_id = bytes.fromhex(msg[1])
                 sender_id = bytes.fromhex(msg[2])
                 self.update_routing_table(sender_id, addr[0], addr[1])
@@ -110,22 +135,18 @@ class DHTNode:
                 response = "NODES|" + "|".join([f"{nid.hex()}:{ip}:{port}" for nid, (ip, port) in closest])
                 self.socket.sendto(response.encode(), addr)
             elif msg[0] == "NODES":
-                # print("Nodes", self.port)
                 for node_info in msg[1:]:
                     nid_hex, ip, port = node_info.split(':')
                     nid = bytes.fromhex(nid_hex)
                     self.update_routing_table(nid, ip, port)
             elif msg[0] == "STORE":
-                # print("Store", self.port)
                 file_hash = bytes.fromhex(msg[1])
                 peer_ip, peer_port = msg[2].split(':')
                 self.store_peer(file_hash, peer_ip, peer_port)
             elif msg[0] == "FIND_PEERS":
-                # print("Find Peers", self.port)
                 file_hash = bytes.fromhex(msg[1])
                 sender_id = bytes.fromhex(msg[2])
                 self.update_routing_table(sender_id, addr[0], addr[1])
-                    
                 peers = self.get_peers(file_hash)
                 if len(peers) > 0:
                     response = "PEERS|" + file_hash.hex() + "|" + "|".join([f"{p.ip}:{p.port}" for p in peers])
@@ -134,7 +155,6 @@ class DHTNode:
                     response = "NODES|" + "|".join([f"{nid.hex()}:{ip}:{port}" for nid, (ip, port) in closest])
                 self.socket.sendto(response.encode(), addr)
             elif msg[0] == "PEERS":
-                # print("Peers", self.port)
                 file_hash = bytes.fromhex(msg[1])
                 for p in msg[2:]:
                     peer_ip, peer_port = p.split(':')
@@ -158,11 +178,6 @@ class DHTNode:
         all_nodes.sort(key=lambda x: self.distance(x[0], target_id))
         return all_nodes[:count]
 
-    # Connect to the dht net using bootstrap node
-    def bootstrap(self, bootstrap_nodes):
-        for node in bootstrap_nodes:
-            self.find_node(self.node_id, node)
-
     # Query a node to find closest nodes to target
     def find_node(self, target_id, addr):
         self.socket.sendto(f"FIND_NODE|{target_id.hex()}|{self.node_id.hex()}".encode(), addr)
@@ -178,19 +193,13 @@ class DHTNode:
             self.storage[file_hash] = []
         self.storage[file_hash].append(PeerInfo(ip, port))
 
+    # Get peers for a specific file
     def get_peers(self, file_hash):
         self.cleanup_storage()
         if file_hash in self.storage:
             return self.storage[file_hash]
         else:
             return []
-
-    def cleanup_storage(self):
-        now = time()
-        for file_hash in self.storage.keys():
-            self.storage[file_hash] = [p for p in self.storage[file_hash] if now - p.last_seen < CLEANUP_REFRESH_INTERVAL]
-            if len(self.storage[file_hash]) == 0:
-                self.storage.pop(file_hash)
     
     # DHT Lookup
     def find_peers(self, file_name):
@@ -202,7 +211,6 @@ class DHTNode:
             for node_id, addr in closest_nodes:
                 if addr not in contacted:
                     self.socket.sendto(f"FIND_PEERS|{file_hash.hex()}|{self.node_id.hex()}".encode(), addr)
-                    msg = f"FIND_PEERS|{file_hash.hex()}|{self.node_id.hex()}".split("|")
                     contacted.add(addr)
                     new_messages += 1
             if new_messages == 0:
@@ -215,89 +223,70 @@ class DHTNode:
                 peers.append((p.ip, p.port))
         return peers
 
+    # Announce a new peer for a file
     def announce_peer(self, file_name, ip, port):
         file_hash = sha1(file_name.encode()).digest()
+        # Find closest nodes
+        while True:
+            new_messages = 0
+            closest_nodes = self.find_closest_nodes(file_hash, 3)
+            for node_id, addr in closest_nodes:
+                if node_id not in self.routing_table and node_id != self.node_id:
+                    self.find_node(file_hash, addr)
+                    new_messages += 1
+            if new_messages == 0:
+                break
+            sleep(2)
+        # Send store message
         for node in self.find_closest_nodes(file_hash, 3):
             self.socket.sendto(f"STORE|{file_hash.hex()}|{ip}:{port}".encode(), node[1])
 
-    # Periodically refresh routing table
-    def refresh(self):
-        while self.running:
-            sleep(ROUTING_REFRESH_INTERVAL)
-            # Refresh a random node
-            rnd_node = self.routing_table[choice(self.routing_table.keys())]
-            self.find_node(rnd_node.id, (rnd_node.ip, rnd_node.port))
-    
-    # Stop running of dht node
-    def shutdown(self): 
-        self.running = False
-        self.socket.close()
 
 # --- Example Usage ---
 if __name__ == "__main__":
-    # Start bootstrap node
-    bootstrap = DHTNode("127.0.0.1", 6881)
-    bootstrap.start()
+    # Start bootstrap nodes
+    bootstrap1 = DHTNode("127.0.0.1", 6881)
+    bootstrap1.start()
+
+    bootstrap2 = DHTNode("127.0.0.1", 6882)
+    bootstrap2.start()
+    bootstrap2.bootstrap((("127.0.0.1", 6881),))
+
 
     # Start regular nodes
-    node1 = DHTNode("127.0.0.1", 6882)
+    node1 = DHTNode("127.0.0.1", 6883)
     node1.start()
     node1.bootstrap((("127.0.0.1", 6881),))
 
-    node2 = DHTNode("127.0.0.1", 6883)
+    node2 = DHTNode("127.0.0.1", 6884)
     node2.start()
     node2.bootstrap((("127.0.0.1", 6881),))
 
+    node3 = DHTNode("127.0.0.1", 6885)
+    node3.start()
+    node3.bootstrap((("127.0.0.1", 6882),))
+
+    node4 = DHTNode("127.0.0.1", 6886)
+    node4.start()
+    node4.bootstrap((("127.0.0.1", 6882),))
+
     # Let them discover each other
-    sleep(5)
-
-    # Now they should know about each other
-    print("\nBootstrap node knows about:")
-    for n in bootstrap.routing_table.keys():
-        node = bootstrap.routing_table[n]
-        print(f"- {node.id.hex()[:8]} at {node.ip}:{node.port}")
-
-    print("\nNode1 knows about:")
-    for n in node1.routing_table.keys():
-        node = node1.routing_table[n]
-        print(f"- {node.id.hex()[:8]} at {node.ip}:{node.port}")
-    
-    print("\nNode2 knows about:")
-    for n in node2.routing_table.keys():
-        node = node2.routing_table[n]
-        print(f"- {node.id.hex()[:8]} at {node.ip}:{node.port}")
+    sleep(2)
     
     # Announcing peers
-    node2.announce_peer("file_name", "127.0.0.1", "6884")
-    node1.announce_peer("file_name2", "127.0.0.1", "6885")
+    node1.announce_peer("file_name", "127.0.0.1", "1")
+    node4.announce_peer("file_name", "127.0.0.1", "2")
 
-    sleep(5)
-
-    # Now what files they know
-    print("\nBootstrap node knows about:")
-    for s in bootstrap.storage.keys():
-        print(s.hex())
-        for i in bootstrap.storage[s]:
-            print(f"- {i.ip}:{i.port}")
-
-    print("\nNode1 knows about:")
-    for s in node1.storage.keys():
-        print(s.hex())
-        for i in node1.storage[s]:
-            print(f"- {i.ip}:{i.port}")
+    sleep(2)
     
-    print("\nNode2 knows about:")
-    for s in node2.storage.keys():
-        print(s.hex())
-        for i in node2.storage[s]:
-            print(f"- {i.ip}:{i.port}")
-    
-    # Find peers for file 2
-    print("\nNode1 finds file 'file_name2'")
-    print(node1.find_peers("file_name2"))
+    # Find peers for file
+    print("\nNode finds file 'file_name'")
+    print(node4.find_peers("file_name"))
 
     # Clean up
-    sleep(5)
-    bootstrap.shutdown()
+    bootstrap1.shutdown()
+    bootstrap2.shutdown()
     node1.shutdown()
     node2.shutdown()
+    node3.shutdown()
+    node4.shutdown()
